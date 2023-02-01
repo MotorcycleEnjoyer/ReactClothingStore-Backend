62a7a911a7bf78edaf201052be1f60df6fcb38d4
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-02-01 16:00:44 -0500

    combine searchByName and searchById

--- helper.js
+++ helper.js
@@ -1,20 +1,34 @@
 const dummyProductDB = require('./dummyProductDB')
 const suggestionDB = require("./Suggestions")
 
-function findSearchSuggestions(searchTerm){
+function genRegex(searchTerm)
+{   
     let regex
     try{
         regex = new RegExp(searchTerm, 'gi');
     }catch(e){
         console.error(e)
-        return []
+        return
     }
+    return regex
+}
 
+function findSearchSuggestions(searchTerm){
+    let regex = genRegex(searchTerm)
     return(
-        suggestionDB.filter(item => {
-            return item.match(regex)
-        })
+        suggestionDB.filter(item => item.match(regex))
     )
 }
 
-module.exports = { findSearchSuggestions }
+function getProductFromProductDatabase(productName, productId){
+    if(productId === undefined){
+        let regex = genRegex(productName)
+    return(
+        dummyProductDB.db.filter(item => item.name.match(regex))
+    )
+    }else{
+        return dummyProductDB.db.filter(item => item.id === productId)
+    }
+}
+
+module.exports = { findSearchSuggestions, getProductFromProductDatabase}
