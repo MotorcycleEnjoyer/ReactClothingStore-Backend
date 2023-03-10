fd5639976c9fc5a74b10d4f0c6d87b98aa04090c
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-03-10 12:37:36 -0500

    generate suggestion list dynamically

--- DummyProductDB.js
+++ DummyProductDB.js
@@ -19,6 +19,47 @@ const db = [
         userSelectedParameters: {},
         amount: 0
     },
+    {
+        details: {
+            id: 2,
+            name: "Specific T Shirt",
+            manufacturerOrBrand: "Someone",
+            typeOfClothing: "T-Shirt",
+            colorOptions: ["red", "pink"],
+            amountInStockPerColor: {
+                    red: 10, 
+                    pink: 2
+            },
+            weight: {grams: 50},
+            price: 5.99,
+            materials: {polyester: "50%", cotton: "50%"},
+        },
+        userSelectedParameters: {},
+        amount: 0
+    },
+    {
+        details: {
+            id: 3,
+            name: "Some T Shirt",
+            manufacturerOrBrand: "Someone",
+            typeOfClothing: "T-Shirt",
+            colorOptions: ["red", "green", "orange", "pink", "cyan", "purple", "yellow", "blue", "pink"],
+            amountInStockPerColor: {
+                    red: 10, 
+                    green: 3,
+                    orange: 0, 
+                    pink: 2
+            },
+            weight: {grams: 50},
+            price: 5.99,
+            materials: {polyester: "50%", cotton: "50%"},
+        },
+        userSelectedParameters: {},
+        amount: 0
+    },
+    
 ]
 
-module.exports = {db}
+const suggestions = db.map(item => item.details.name)
+
+module.exports = {db, suggestions}
