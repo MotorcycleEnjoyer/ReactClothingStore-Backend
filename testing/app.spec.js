2aa33c33059a0213f6c2fd5226d721d628bbd34e
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-06 11:51:46 -0400

    create tests for suggestions endpoint
    I checked happy condition and error conditions:
    - undefined
    - len 0, len >50
    - invalid chars

--- testing/app.spec.js
+++ testing/app.spec.js
@@ -1,11 +1,5 @@
 const app = require("../Server")
 const request = require('supertest');
-/* const express = require("express")
-const fakeApp = express()
-
-fakeApp.get("/", (req, res) => {
-  res.json({hello: "world"})
-}) */
 
 test('GET /test responds with hello world', async () => {
   // arrange
@@ -18,3 +12,45 @@ test('GET /test responds with hello world', async () => {
   expect(response.statusCode).toEqual(200);
   expect(response.body).toEqual({ hello: 'world' })
 });
+
+test('POST /backend/suggestions responds with array of suggestions', async () => {
+  // arrange
+  const api = request(app)
+  const endpoint = "/backend/suggestions"
+
+  const payload = {searchTerm: "Shirt"}
+  const expectedResponse = ["Generic T Shirt", "Specific T Shirt", "Some T Shirt"]
+
+  const fiftyCharPayload = {searchTerm: "LoremIpsumLoremIpsumLoremIpsumLoremIpsumLoremIpsum"}
+  
+  // act
+  const response = await api.post(endpoint).send(payload)
+  const fiftyCharResponse = await api.post(endpoint).send(fiftyCharPayload)
+
+  // assert
+  expect(response.body).toEqual(expectedResponse)
+  expect(fiftyCharResponse.body).toEqual([])
+})
+
+test('POST /backend/suggestions searchTerm(undefined || len>50 || len==0 || InvalidChars) returns error 500', async () => {
+  // arrange
+  const api = request(app)
+  const endpoint = "/backend/suggestions"
+
+  const undefinedPayload = undefined
+  const fiftyOneCharPayload = {searchTerm: "LoremIpsumLoremIpsumLoremIpsumLoremIpsumLoremIpsum5"}
+  const nonNumericAlphabeticPayload = {searchTerm: "abc123@#$%^&!@)((*"}
+  const emptyPayload = {searchTerm: ""}
+
+  // act
+  const undefinedResponse = await api.post(endpoint).send(undefinedPayload)
+  const fiftyOneCharReponse = await api.post(endpoint).send(fiftyOneCharPayload)
+  const invalidCharResponse = await api.post(endpoint).send(nonNumericAlphabeticPayload)
+  const emptyResponse = await api.post(endpoint).send(emptyPayload)
+
+  // assert
+  expect(undefinedResponse.statusCode).toEqual(500)
+  expect(fiftyOneCharReponse.statusCode).toEqual(500)
+  expect(invalidCharResponse.statusCode).toEqual(500)
+  expect(emptyResponse.statusCode).toEqual(500)
+})
