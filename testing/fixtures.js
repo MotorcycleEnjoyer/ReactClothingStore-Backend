23a2816d4b78b4d94e61dde396cb9dc69cce05cd
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-06 15:15:20 -0400

    add shoppingCart AFTER adding 1 sample item

--- testing/fixtures.js
+++ testing/fixtures.js
@@ -133,4 +133,57 @@ const shoppingCarts = {
     }
 }
 
-module.exports = {searchResults, shoppingCarts}
+const addToCart = {
+    "sampleOneRequest": {
+        productId: 0,
+        newUserChoices: {
+            color: "red",
+            size: "L",
+            sexCategory: "M",
+            ageCategory: "adults"
+        },
+        amount: 1
+    },
+    "sampleOneResponse": {
+        "shoppingCart": [
+            {
+                "details": {
+                    "id": 0,
+                    "name": "Generic T Shirt",
+                    "manufacturerOrBrand": "T-Shirt-CO",
+                    "typeOfClothing": "T-Shirt",
+                    "colorOptions": [
+                        "red",
+                        "green",
+                        "orange",
+                        "pink"
+                    ],
+                    "amountInStockPerColor": {
+                        "red": 10,
+                        "green": 3,
+                        "orange": 0,
+                        "pink": 2
+                    },
+                    "weight": {
+                        "grams": 50
+                    },
+                    "price": 5.99,
+                    "materials": {
+                        "polyester": "50%",
+                        "cotton": "50%"
+                    }
+                },
+                "userSelectedParameters": {
+                    "color": "red",
+                    "size": "L",
+                    "sexCategory": "M",
+                    "ageCategory": "adults"
+                },
+                "amount": 1
+            }
+        ],
+        "type": "anonymous"
+    }
+}
+
+module.exports = {searchResults, shoppingCarts, addToCart}
