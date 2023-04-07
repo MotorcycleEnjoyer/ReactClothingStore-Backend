985c78c7131e7cd8b03b69019a87618804fa9740
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-07 16:19:57 -0400

    add user variant of sample addtocart

--- testing/fixtures.js
+++ testing/fixtures.js
@@ -183,6 +183,46 @@ const addToCart = {
             }
         ],
         "type": "anonymous"
+    },
+    "sampleOneUserResponse": {
+        "shoppingCart": [
+            {
+                "details": {
+                    "id": 0,
+                    "name": "Generic T Shirt",
+                    "manufacturerOrBrand": "T-Shirt-CO",
+                    "typeOfClothing": "T-Shirt",
+                    "colorOptions": [
+                        "red",
+                        "green",
+                        "orange",
+                        "pink"
+                    ],
+                    "amountInStockPerColor": {
+                        "red": 10,
+                        "green": 3,
+                        "orange": 0,
+                        "pink": 2
+                    },
+                    "weight": {
+                        "grams": 50
+                    },
+                    "price": 5.99,
+                    "materials": {
+                        "polyester": "50%",
+                        "cotton": "50%"
+                    }
+                },
+                "userSelectedParameters": {
+                    "color": "red",
+                    "size": "L",
+                    "sexCategory": "M",
+                    "ageCategory": "adults"
+                },
+                "amount": 1
+            }
+        ],
+        "type": "user"
     }
 }
 
