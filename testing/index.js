58b59e68d03971b6082da653e532af50312d09d7
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-12 15:36:21 -0400

    make port conditional by NODE_ENV

--- testing/index.js
+++ testing/index.js
@@ -1,7 +1,14 @@
-const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv")
+dotenv.config()
+let port
+if(process.env.NODE_ENV === "production") {
+  port = process.env.PRODUCTION_PORT
+} else {
+  port = process.env.TESTING_PORT
+}
 const app = require("../Server")
 
-app.listen(PORT, (e) => {
+app.listen(port, (e) => {
   if (e) throw err;
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${port}`);
 });
