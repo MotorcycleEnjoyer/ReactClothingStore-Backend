a2a58f0eede55ebec86f12843483eeaa0bbfe8b8
MotorcycleEnjoyer <MotorcycleEnjoyer@Proton.me>
2023-06-11 13:05:00 -0400

    add ratelimit to / endpoint

--- server-rewrite/makeApp.js
+++ server-rewrite/makeApp.js
@@ -1,14 +1,36 @@
 const express = require("express")
 const uuidv4 = require('uuid').v4
+const { RateLimiterMemory } = require("rate-limiter-flexible")
 
 function makeApp (database) {
+    const opts = {
+        points: 11,
+        duration: 60
+    }
+    const rateLimiter = new RateLimiterMemory(opts)
     const app = express()
     app.use(express.json())
 
     const sessions = {}
 
     app.get("/", (req, res) => {
-        res.json("Hello World!")
+        const ipAddress = req.socket.remoteAddress;
+        rateLimiter.consume(ipAddress, 1)
+        .then((rateLimiterRes) => {
+            // Allowed
+            const headers = {
+                "Retry-After": rateLimiterRes.msBeforeNext / 1000,
+                "X-RateLimit-Limit": opts.points,
+                "X-RateLimit-Remaining": rateLimiterRes.remainingPoints,
+                "X-RateLimit-Reset": new Date(Date.now() + rateLimiterRes.msBeforeNext)
+            }
+            res.header(headers)
+            return res.json("Hello World!")
+          })
+          .catch((rej) => {
+            // Blocked
+            return res.status(429).json("Too many requests!!!")
+          });   
     })
 
     app.get("/api/shoppingCart", (req, res) => {
