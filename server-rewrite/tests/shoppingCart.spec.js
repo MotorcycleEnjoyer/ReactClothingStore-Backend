bcb5fe6da4b3da45fbc9dcf5faa65c6e6f924190
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-06-12 13:10:50 -0400

    add tests for invalid item

--- server-rewrite/tests/shoppingCart.spec.js
+++ server-rewrite/tests/shoppingCart.spec.js
@@ -8,7 +8,7 @@ describe("/api/shoppingCart", () => {
     const endpoint = "/api/shoppingCart"
     const fixtureCookie = sessionFixtures.sessionToken
 
-    describe("GET", () => {
+    describe("<<[GET]>>", () => {
         test("Returns object with shopping cart and user login status", async () => {
             const api = request(app)
 
@@ -39,8 +39,8 @@ describe("/api/shoppingCart", () => {
         test("Does NOT return a cookie if session is found on server", async () => {
             const api = request(app)
             const response = await api.get(endpoint)
-            const cookieHeader = response.headers["set-cookie"]
-            const validCookie = cookieHeader[0].split("=")[0]
+            const cookieHeader = response.headers["set-cookie"][0]
+            const validCookie = cookieHeader.split("=")[0]
 
             const testResponse = await api.get(endpoint).set("Cookie", validCookie)
             const testCookie = testResponse.headers["set-cookie"]
@@ -68,36 +68,83 @@ describe("/api/shoppingCart", () => {
         }) */
     })
 
-    describe("POST", () => {
+    describe("<<[POST]>>", () => {
         // This is adding to cart
 
 /*         test("If session exists, returns cart with item pushed", async () => {
             const api = request(sessionApp)
         }) */
 
-        describe("Bad Actions", () => {
+        describe("[Bad Actions]", () => {
             test("No cookie, returns new cart with that item and a cookie", async () => {
-                const api = request(appWithOneActiveSession)
+                const api = request(app)
                 const payload = {
                     itemId: 1
                 }
 
                 const response = await api.post(endpoint).send(payload)
-                const responseCookie = response.headers["set-cookie"]
-                const cookie = responseCookie[0].split("=")[0]
+                const responseCookie = response.headers["set-cookie"][0]
+                const cookie = responseCookie.split("=")[0]
 
                 expect(response.body).toStrictEqual(cartFixtures.itemOneCart)
                 expect(cookie).toBeDefined()
             })
-            test.todo("Cookie not found in sessions, returns new cart with that item and a cookie")
-            test.todo("No item to add to cart, returns status code 400")
-            test.todo("Invalid item to add to cart, returns 400")
+            test("Session not found, returns new cart with that item and a cookie", async () => {
+                const api = request(app)
+                const payload = {
+                    itemId: 1
+                }                
+
+                const response = await api.post(endpoint).send(payload).set("Cookie", fixtureCookie)
+                const responseCookie = response.headers["set-cookie"][0]
+                const cookie = responseCookie.split("=")[0]
+                
+                expect(response.body).toStrictEqual(cartFixtures.itemOneCart)
+                expect(cookie).toBeDefined()
+            })
+            test("No item to add to cart, returns status code 400", async () => {
+                const api = request(appWithOneActiveSession)
+
+                const response = await api.post(endpoint).set("Cookie", fixtureCookie)
+
+                expect(response.status).toBe(400)
+            })
+            describe("(Invalid item to add to cart, returns 400)", () => {
+                test("itemId is undefined", async () => {
+                    const api = request(appWithOneActiveSession)
+                    const payload = { itemId: undefined }
+    
+                    const response = await api.post(endpoint).send(payload).set("Cookie", fixtureCookie)
+                    expect(response.status).toBe(400)                    
+                })
+                test("itemId is null", async () => {
+                    const api = request(appWithOneActiveSession)
+                    const payload = { itemId: null }
+                    
+                    const response = await api.post(endpoint).send(payload).set("Cookie", fixtureCookie)
+                    expect(response.status).toBe(400)
+                })
+                test("itemId is an object", async () => {
+                    const api = request(appWithOneActiveSession)
+                    const payload = { itemId: { id: 12345 } }
+    
+                    const response = await api.post(endpoint).send(payload).set("Cookie", fixtureCookie)
+                    expect(response.status).toBe(400)
+                })
+                test("itemId is an array", async () => {
+                    const api = request(appWithOneActiveSession)
+                    const payload = { itemId: [1, 2] }
+    
+                    const response = await api.post(endpoint).send(payload).set("Cookie", fixtureCookie)
+                    expect(response.status).toBe(400)
+                })
+            })
             test.todo("Invalid amount of item to add to cart, returns 400")
             test.todo("Invalid parameters on item (ex: color), returns 400")
             test.todo("Requests exceed 10/min, returns 429")
         })
 
-        describe("Bad Server Situations", () => {
+        describe("[Bad Server Situations]", () => {
             test.todo("Item of current configuration is out of stock, returns 500")
             test.todo("Cannot reach Database, add to cart fails, returns 500")
             test.todo("Database fails to add to cart, returns 500")
@@ -105,9 +152,9 @@ describe("/api/shoppingCart", () => {
 
     })
 
-    describe("PUT", () => {
+    describe("<<[PUT]>>", () => {
         // This is editing cart item
-        describe("Bad actions", () => {
+        describe("[Bad actions]", () => {
             test.todo("No cookie, returns status code 400")
             test.todo("Cookie not found in sessions, returns status code 400")
             test.todo("No item, returns status code 400")
@@ -116,7 +163,7 @@ describe("/api/shoppingCart", () => {
             test.todo("Invalid parameters (ex: color) on edit, returns 400")
         })
 
-        describe("Bad Server Situations", () => {
+        describe("[Bad Server Situations]", () => {
             test.todo("Item of edit configuration is out of stock, returns 500")
             test.todo("Cannot reach database, edit cart fails, returns 500")
             test.todo("Database fails to edit item, returns 500")
@@ -124,16 +171,16 @@ describe("/api/shoppingCart", () => {
 
     })
 
-    describe("DELETE", () => {
+    describe("<<[DELETE]>>", () => {
         // This is deleting cart item or whole cart
-        describe("Bad Actions", () => {
+        describe("[Bad Actions]", () => {
             test.todo("No cookie, returns 400")
             test.todo("Cookie not found in sessions, returns 400")
             test.todo("No index to delete, returns 400")
             test.todo("Invalid index to delete, returns 400")
         })
 
-        describe("Bad Server Situations", () => {
+        describe("[Bad Server Situations]", () => {
             test.todo("Index does not exist, returns 500")
             test.todo("Cannot reach database, delete cart item fails, returns 500")
             test.todo("Database fails to delete item, returns 500")
