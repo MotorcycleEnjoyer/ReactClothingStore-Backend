36454f5090a241505948186579250ee3376e66c1
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-06-12 12:21:13 -0400

    add a cart and a session fixture

--- server-rewrite/fixtures.js
+++ server-rewrite/fixtures.js
@@ -2,7 +2,23 @@ const cartFixtures = {
     intitialCart: {
         loginStatus: "anon",
         shoppingCart: []
+    },
+    itemOneCart: {
+        loginStatus: "anon",
+        shoppingCart: [{
+            itemId: 1
+        }]
     }
 }
 
-module.exports = { cartFixtures }
+const sessionFixtures = {
+    sessionToken: "0904437e-95db-4372-82ea-0310812bcf1a",
+    oneSession: {
+        "0904437e-95db-4372-82ea-0310812bcf1a": {
+            loginStatus: "anon",
+            shoppingCart: []
+        }
+    }
+}
+
+module.exports = { cartFixtures, sessionFixtures }
