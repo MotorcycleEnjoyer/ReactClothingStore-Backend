fb6fa9db2f745c623b8ca04203850200dfd05387
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-06-21 13:49:07 -0400

    update fixtures

--- server-rewrite/fixtures.js
+++ server-rewrite/fixtures.js
@@ -24,68 +24,44 @@ const cartFixtures = {
                 size: "medium",
             }
         }]
+    },
+    itemTwoCart: {
+        loginStatus: "anon",
+        shoppingCart: [{
+            itemId: 1,
+            amount: 2,
+            params: {
+                color: "gray",
+                size: "medium",
+            }
+        },
+        {
+            itemId: 2,
+            amount: 3,
+            params: {
+                color: "gray",
+                size: "medium",
+            }
+        }
+    ]
+    },
+    finalCart: {
+        loginStatus: "anon",
+        shoppingCart: [{
+            itemId: 1,
+            amount: 10,
+            params: {
+                color: "gray",
+                size: "medium",
+            }
+        }]
     }
 }
 
 const sessionFixtures = {
     sessionToken: "0904437e-95db-4372-82ea-0310812bcf1a",
     oneSession: {
-        "0904437e-95db-4372-82ea-0310812bcf1a": {
-            loginStatus: "anon",
-            shoppingCart: []
-        }
-    },
-    sessionWithOneItem: {
-        "0904437e-95db-4372-82ea-0310812bcf1a": {
-            loginStatus: "anon",
-            shoppingCart: [{
-                itemId: 1,
-                amount: 1,
-                params: {
-                    color: "gray",
-                    size: "medium",
-                }
-            }]
-        }
-    },
-    sessionWithManyItems: {
-        "0904437e-95db-4372-82ea-0310812bcf1a": {
-            loginStatus: "anon",
-            shoppingCart: [
-                {
-                    itemId: 0,
-                    amount: 1,
-                    params: {
-                        color: "gray",
-                        size: "medium",
-                    }
-                },
-                {
-                    itemId: 1,
-                    amount: 2,
-                    params: {
-                        color: "yellow",
-                        size: "medium",
-                    }
-                },
-                {
-                    itemId: 2,
-                    amount: 3,
-                    params: {
-                        color: "orange",
-                        size: "medium",
-                    }
-                },
-                {
-                    itemId: 3,
-                    amount: 5,
-                    params: {
-                        color: "pink",
-                        size: "medium",
-                    }
-                },
-            ]
-        }
+        "0904437e-95db-4372-82ea-0310812bcf1a": {type: "anon"}
     }
 }
 
