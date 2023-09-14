a61a2081c240e0cef55d623d1a2e21f029ca17e7
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-09-14 15:54:03 -0400

    re-add dummy products

---
+++ dummyProductDb.js
@@ -0,0 +1,74 @@
+const dummyProducts = [
+    {
+        details: {
+            id: 0,
+            name: "Generic T Shirt",
+            manufacturerOrBrand: "T-Shirt-CO",
+            typeOfClothing: "T-Shirt",
+            colorOptions: ["red", "green", "orange", "pink"],
+            amountInStockPerColor: {
+                red: 10,
+                green: 3,
+                orange: 0,
+                pink: 2,
+            },
+            weight: { grams: 50 },
+            price: 5.99,
+            materials: { polyester: "50%", cotton: "50%" },
+            imageName: "t-shirt-preview.png",
+        },
+        userSelectedParameters: {},
+        amount: 0,
+    },
+    {
+        details: {
+            id: 1,
+            name: "Specific T Shirt",
+            manufacturerOrBrand: "Someone",
+            typeOfClothing: "T-Shirt",
+            colorOptions: ["red", "pink"],
+            amountInStockPerColor: {
+                red: 10,
+                pink: 2,
+            },
+            weight: { grams: 50 },
+            price: 10.99,
+            materials: { polyester: "50%", cotton: "50%" },
+            imageName: "t-shirt-preview.png",
+        },
+        userSelectedParameters: {},
+        amount: 0,
+    },
+    {
+        details: {
+            id: 2,
+            name: "Some T Shirt",
+            manufacturerOrBrand: "Someone",
+            typeOfClothing: "T-Shirt",
+            colorOptions: [
+                "red",
+                "green",
+                "orange",
+                "pink",
+                "cyan",
+                "purple",
+                "yellow",
+                "blue",
+            ],
+            amountInStockPerColor: {
+                red: 10,
+                green: 3,
+                orange: 0,
+                pink: 2,
+            },
+            weight: { grams: 50 },
+            price: 3.99,
+            materials: { polyester: "50%", cotton: "50%" },
+            imageName: "t-shirt-preview.png",
+        },
+        userSelectedParameters: {},
+        amount: 0,
+    },
+];
+const suggestions = dummyProducts.map((item) => item.details.name);
+module.exports = { dummyProducts, suggestions };
