79f3f0492b57e9f417142b697f7b279ac40933b9
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-10-03 15:12:37 -0400

    rename variables for clarity, add couple user tests

--- __spec__/mongoDB/mongoDbCarts.spec.js
+++ __spec__/mongoDB/mongoDbCarts.spec.js
@@ -32,19 +32,30 @@ describe("Guest Cart Model", () => {
         );
     });
 
+    test("Expect getUser to return an existing Guest, given existing session token", async () => {
+        const payload = getParams();
+
+        const response = await DatabaseMethods.getUser(sessionToken);
+
+        expect(response).toEqual(
+            expect.objectContaining({
+                __v: expect.anything(),
+                _id: expect.anything(),
+                sessionToken: payload.sessionToken,
+                shoppingCart: [],
+            })
+        );
+    });
+
     test("Expect adding to empty cart to push 1 item", async () => {
         const setupPayload = getParams();
-        const setupResponse = await DatabaseMethods.createAndReturnGuest(
-            setupPayload
-        );
         const newProduct = setupPayload.newProduct;
 
         await DatabaseMethods.addToCart(newProduct, sessionToken);
-        const finalState = (await DatabaseMethods.getUser(sessionToken))
-            .shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(finalState.length).toBe(1);
-        expect(finalState[0]).toEqual(
+        expect(shoppingCart.length).toBe(1);
+        expect(shoppingCart[0]).toEqual(
             expect.objectContaining({
                 itemId: newProduct.itemId,
                 amount: newProduct.amount,
@@ -60,11 +71,10 @@ describe("Guest Cart Model", () => {
         const { newProduct, sessionToken } = getParams();
 
         await DatabaseMethods.addToCart(newProduct, sessionToken);
-        const finalState = (await DatabaseMethods.getUser(sessionToken))
-            .shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(finalState.length).toBe(1);
-        expect(finalState[0]).toEqual(
+        expect(shoppingCart.length).toBe(1);
+        expect(shoppingCart[0]).toEqual(
             expect.objectContaining({
                 itemId: newProduct.itemId,
                 amount: 1 + newProduct.amount,
@@ -82,11 +92,10 @@ describe("Guest Cart Model", () => {
 
         await DatabaseMethods.addToCart(newProduct, sessionToken);
 
-        const finalState = (await DatabaseMethods.getUser(sessionToken))
-            .shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(finalState.length).toBe(2);
-        expect(finalState[1]).toEqual(
+        expect(shoppingCart.length).toBe(2);
+        expect(shoppingCart[1]).toEqual(
             expect.objectContaining({
                 itemId: newProduct.itemId,
                 amount: newProduct.amount,
@@ -98,16 +107,12 @@ describe("Guest Cart Model", () => {
         );
     });
 
-    test("Expect adding invalid item to return nothing", async () => {
-        const newProduct = "blablabla";
-    });
-
     test("Expect removing item to do so, if it exists", async () => {
         await DatabaseMethods.deleteCartItem(0, sessionToken);
 
-        const cart = (await DatabaseMethods.getUser(sessionToken)).shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(cart[0]).toStrictEqual(
+        expect(shoppingCart[0]).toStrictEqual(
             expect.objectContaining({
                 itemId: 2,
                 amount: 1,
@@ -122,18 +127,17 @@ describe("Guest Cart Model", () => {
     test("Expect removing final item, to return empty cart", async () => {
         await DatabaseMethods.deleteCartItem(0, sessionToken);
 
-        const cart = (await DatabaseMethods.getUser(sessionToken)).shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(cart[0]).toBe(undefined);
+        expect(shoppingCart[0]).toBe(undefined);
     });
 
     test("Expect removing from empty cart, to return empty cart", async () => {
         const response = await DatabaseMethods.deleteCartItem(0, sessionToken);
-        console.log(response);
 
-        const cart = (await DatabaseMethods.getUser(sessionToken)).shoppingCart;
+        const { shoppingCart } = await DatabaseMethods.getUser(sessionToken);
 
-        expect(cart[0]).toBe(undefined);
+        expect(shoppingCart[0]).toBe(undefined);
     });
 
     function getParams(override = {}) {
@@ -167,7 +171,7 @@ describe("User Cart Model", () => {
     });
 
     test("Expect createAndReturnUser to return a new User", async () => {
-        const payload = getParams();
+        const payload = getCreds();
 
         const response = await DatabaseMethods.createAndReturnUser(payload);
 
@@ -183,29 +187,46 @@ describe("User Cart Model", () => {
         );
     });
 
-    // test("Expect adding to empty cart to push 1 item", async () => {
-    //     const setupPayload = getParams();
-    //     const setupResponse = await DatabaseMethods.createAndReturnGuest(
-    //         setupPayload
-    //     );
-    //     const newProduct = setupPayload.newProduct;
+    test("Expect getUser to return an existing User", async () => {
+        const payload = getCreds();
 
-    //     await DatabaseMethods.addToCart(newProduct, sessionToken);
-    //     const finalState = (await DatabaseMethods.getUser(sessionToken))
-    //         .shoppingCart;
+        const response = await DatabaseMethods.getUser({
+            username: payload.username,
+        });
 
-    //     expect(finalState.length).toBe(1);
-    //     expect(finalState[0]).toEqual(
-    //         expect.objectContaining({
-    //             itemId: newProduct.itemId,
-    //             amount: newProduct.amount,
-    //             params: expect.objectContaining({
-    //                 color: "gray",
-    //                 size: "medium",
-    //             }),
-    //         })
-    //     );
-    // });
+        expect(response).toEqual(
+            expect.objectContaining({
+                __v: expect.anything(),
+                _id: expect.anything(),
+                username: payload.username,
+                password: payload.password,
+                loginStatus: "user",
+                shoppingCart: [],
+            })
+        );
+    });
+
+    test("Expect adding to empty cart to push 1 item", async () => {
+        const { username } = getCreds();
+        const { newProduct } = getItem();
+
+        await DatabaseMethods.addToCart(newProduct, {
+            username,
+        });
+        const { shoppingCart } = await DatabaseMethods.getUser({ username });
+
+        expect(shoppingCart.length).toBe(1);
+        expect(shoppingCart[0]).toEqual(
+            expect.objectContaining({
+                itemId: newProduct.itemId,
+                amount: newProduct.amount,
+                params: expect.objectContaining({
+                    color: "gray",
+                    size: "medium",
+                }),
+            })
+        );
+    });
 
     // test("Expect adding duplicate item to increment existing in cart", async () => {
     //     const { newProduct, sessionToken } = getParams();
@@ -287,10 +308,28 @@ describe("User Cart Model", () => {
     //     expect(cart[0]).toBe(undefined);
     // });
 
-    function getParams(override = {}) {
+    function getCreds(override = {}) {
         const defaultParams = {
             username: "abcdefg",
             password: "abcdefg",
+        };
+
+        const params = { ...defaultParams, ...override };
+
+        return params;
+    }
+
+    function getItem(override = {}) {
+        const defaultParams = {
+            sessionToken: "0904437e-95db-4372-82ea-0310812bcf1a",
+            newProduct: {
+                itemId: 1,
+                amount: 1,
+                params: {
+                    color: "gray",
+                    size: "medium",
+                },
+            },
         };
 
         const params = { ...defaultParams, ...override };
